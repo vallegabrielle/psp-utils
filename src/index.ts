8b5f2b32c0077c11c5aa2a0baf6f944502eb3c71
Pedro Machado <pedro.machado@liferay.com>
2024-04-17 09:08:21 -0300

    chore: express setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-const message = "Hello, World!"
+import * as express from "express"
+import { Request, Response } from "express"
+
+const app = express()
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("services-migrator")
+})
+
+const PORT = 3000
+
+app.listen(PORT, () => console.log(`🌐 Server is running on port ${PORT}`))

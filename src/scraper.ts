0d21696416ac49f43062579e8d4631f00ab124ff
Pedro Machado <pedro.machado@liferay.com>
2024-04-17 13:53:23 -0300

    feat: returning service data in scraper

--- src/scraper.ts
+++ src/scraper.ts
@@ -5,20 +5,49 @@ type ScraperProps = {
   url: string
 }
 
+type ServiceInfo = {
+  url: string
+  image: string
+  isExternalLink: boolean
+}[]
+
 export async function scraper({ url }: ScraperProps) {
   try {
     const res = await axios.get(url)
-    if (res.status === 200) {
-      const html = res.data
-      const $ = cheerio.load(html)
 
-      const hasServices = $('h2:contains("SERVIÇOS")').html()
+    if (res.status !== 200) return
 
-      if (hasServices) {
-        return $("div.panel.panel-default.panel-content.panel-notices").html()
-      }
-    }
-    return
+    const html = res.data
+    const $ = cheerio.load(html)
+
+    const hasServices = $('h2:contains("SERVIÇOS")').html()
+
+    if (!hasServices) return
+
+    const servicesSection = $(
+      "div.panel.panel-default.panel-content.panel-notices"
+    ).html()
+
+    if (!servicesSection) return
+
+    const $servicesSectionHtml = cheerio.load(servicesSection)
+
+    const serviceInfo: ServiceInfo = []
+
+    $servicesSectionHtml(".col-md-6.col-xs-12").each((index, element) => {
+      const url = $(element).find("a").attr("href")
+      const image = $(element).find("img").attr("src")
+
+      if (!url || !image) return
+
+      serviceInfo.push({
+        url,
+        image,
+        isExternalLink: !url.includes("prefeitura.sp.gov.br"),
+      })
+    })
+
+    return serviceInfo
   } catch (err) {
     console.log("Error @ scraper:", err)
   }

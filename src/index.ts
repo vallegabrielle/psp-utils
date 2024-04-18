992943db6463c090835750457670b8e28dd24a2e
Pedro Machado <pedro.machado@liferay.com>
2024-04-18 11:30:12 -0300

    feat: service to get all servicos

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import * as express from "express"
 import { Request, Response } from "express"
 import { scraper } from "@/scraper"
+import { getSecretarias } from "@/services/get-secretarias"
+import { getServicos } from "@/services/get-servicos"
 
 const app = express()
 
@@ -15,6 +17,27 @@ app.get("/", async (req: Request, res: Response) => {
   }
 
   res.send(scraperRes)
+})
+
+app.get("/servicos", async (req: Request, res: Response) => {
+  const url = req.query.site as string
+
+  const secretarias = await getSecretarias(url)
+
+  if (!secretarias) {
+    res.status(404).send("Secretarias section not found :(")
+    return
+  }
+
+  for (const secretaria of secretarias) {
+    const servicos = await getServicos(secretaria.link)
+
+    if (!servicos) return
+
+    secretaria.servicos = servicos
+  }
+
+  res.send(secretarias)
 })
 
 const PORT = 3000
